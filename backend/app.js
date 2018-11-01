164ca983b410647da72303e3d9e9c400ed56e699
Panagiotis <1qk231@gmail.com>
2018-11-01 19:28:50 +0200

    add CORS middleware

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,16 @@ mongoose.connect(
   }
 );
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use(bodyParser.json());
 
 app.use(passport.initialize());
 
