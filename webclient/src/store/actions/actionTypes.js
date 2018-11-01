4cd7ac2f3abfa35aac7f3b526d040c47b06174e8
Panagiotis <1qk231@gmail.com>
2018-11-01 19:45:27 +0200

    add actionTypes

---
+++ webclient/src/store/actions/actionTypes.js
@@ -0,0 +1,8 @@
+export const AUTH_START = "AUTH_START";
+export const AUTH_SUCCESS = "AUTH_SUCCESS";
+export const AUTH_FAIL = "AUTH_FAIL";
+export const AUTH_LOGOUT = "AUTH_LOGOUT";
+
+export const START_TIMER = "START_TIMER";
+export const DEC_TIMER = "DEC_TIMER";
+export const STOP_TIMER = "STOP_TIMER";
