fa55ec42e6e8833fda9513eb0a3a490f40fc3249
Panagiotis <1qk231@gmail.com>
2018-11-07 20:43:43 +0200

    add board reducer to main

--- webclient/src/index.js
+++ webclient/src/index.js
@@ -9,12 +9,14 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import pomodoroReducer from "./store/reducers/pomodoro";
 import authReducer from "./store/reducers/auth";
+import boardReducer from "./store/reducers/board";
 import "./vendors/materialize/sass/materialize.scss";
 import "./scss/_index.scss";
 
 const rootReducer = combineReducers({
   pomodoro: pomodoroReducer,
-  auth: authReducer
+  auth: authReducer,
+  board: boardReducer
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
