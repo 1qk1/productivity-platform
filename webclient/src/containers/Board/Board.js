ac1878c514b03349906114dc36c071fa5a6b6a5f
Panagiotis <1qk231@gmail.com>
2018-11-07 20:44:42 +0200

    add redux and some other stuff

--- webclient/src/containers/Board/Board.js
+++ webclient/src/containers/Board/Board.js
@@ -1,8 +1,9 @@
-import React, { Component, Fragment } from "react";
+import React, { PureComponent, Fragment } from "react";
 import List from "../../components/UI/Board/List/List";
-import uuidv4 from "uuid/v4";
 import { DragDropContext } from "react-dnd";
 import HTML5Backend from "react-dnd-html5-backend";
+import { connect } from "react-redux";
+import * as actionTypes from "../../store/actions/actionTypes";
 
 import "./Board.scss";
 
@@ -10,21 +11,9 @@ export const itemTypes = {
   CARD: "card"
 };
 
-class Board extends Component {
-  state = {
-    board: []
-  };
-
-  newCard = index => {
-    if (this.state.board.length === 0) return;
-    const newBoard = [...this.state.board];
-    newBoard[index].push({ id: uuidv4(), text: "new card" });
-    this.setState({ board: newBoard });
-  };
-
-  newList = () => {
-    const newBoard = [...this.state.board, []];
-    this.setState({ board: newBoard });
+class Board extends PureComponent {
+  newCardHandler = event => {
+    event.preventDefault();
   };
 
   render() {
@@ -32,15 +21,22 @@ class Board extends Component {
       <Fragment>
         <div className="Board scrollbar">
           {/* render list */}
-          {this.state.board.map((list, index) => (
+          {this.props.board.lists.map((list, index) => (
             <List
-              addCard={this.newCard}
+              newCardHandler={this.newCardHandler}
+              isAdding={this.props.board.settings.adding.includes(index)}
+              toggleAdding={this.props.toggleAdding}
+              changeList={this.props.changeList}
+              addCard={this.props.addCard}
               key={`list-${Math.random()}`}
               list={list}
               index={index}
             />
           ))}
-          <button className="btn btn-small Add-List-Btn" onClick={this.newList}>
+          <button
+            className="btn btn-small Add-List-Btn"
+            onClick={this.props.addList}
+          >
             Add a List
           </button>
         </div>
@@ -49,4 +45,20 @@ class Board extends Component {
   }
 }
 
-export default DragDropContext(HTML5Backend)(Board);
+const mapStateToProps = state => ({
+  board: state.board.board
+});
+const mapDispatchToProps = dispatch => ({
+  toggleAdding: listId => dispatch({ type: actionTypes.TOGGLE_ADDING, listId }),
+  addCard: listId => dispatch({ type: actionTypes.ADD_CARD, listId }),
+  addList: () => dispatch({ type: actionTypes.ADD_LIST }),
+  changeList: (prevList, listToMoveTo, cardId) =>
+    dispatch({ type: actionTypes.CHANGE_LIST, prevList, listToMoveTo, cardId })
+});
+
+export default DragDropContext(HTML5Backend)(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Board)
+);
