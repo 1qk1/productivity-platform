38805b98b0e15a9e9959a1f01a9746481f3a4179
Panagiotis <1qk231@gmail.com>
2018-11-09 19:21:20 +0200

    add editable title functionality

--- webclient/src/components/UI/Board/List/List.js
+++ webclient/src/components/UI/Board/List/List.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import Card from "../Card/Card";
 import { DropTarget } from "react-dnd";
 import NewCard from "../NewCard/NewCard";
+import EditableTitle from "./EditableTitle";
 
 import "./List.scss";
 
@@ -22,29 +23,75 @@ function collect(connect, monitor) {
   };
 }
 
-const list = props => {
-  return props.connectDropTarget(
-    <div className="List scrollbar">
-      <div className="List-Header">
-        <p className="List-Title">title</p>
-        <button
-          className="Add-Card-Button"
-          onClick={() => props.toggleAdding(props.index)}
-        >
-          <i className="fas fa-plus" />
-        </button>
-      </div>
-      <div className="List-Cards">
-        {props.isAdding ? (
-          <NewCard onSubmit={props.newCardHandler} listIndex={props.index} />
-        ) : null}
-        {props.list.map(card => (
-          <Card key={`card-${card.id}`} {...card} />
-        ))}
-        {props.isOver ? <div className="Card" /> : null}
-      </div>
-    </div>
-  );
-};
+class List extends PureComponent {
+  state = {
+    adding: false,
+    newCardText: "",
+    editingTitle: false,
+    newTitle: ""
+  };
 
-export default DropTarget("CARD", listTarget, collect)(list);
+  toggleProp = prop => {
+    this.setState(() => ({ [prop]: !this.state[prop] }));
+  };
+
+  onCardChangeHandler = e => {
+    this.setState({ newCardText: e.target.value });
+  };
+
+  onSubmitCardHandler = event => {
+    event.preventDefault();
+    this.props.addCard(this.props.index, this.state.newCardText);
+    this.setState({ adding: false, newCardText: "" });
+  };
+
+  onSubmitTitleHandler = event => {
+    event.preventDefault();
+    this.props.changeListTitleHandler(this.state.newTitle, this.props.index);
+    this.props.changeListTitle(this.props.index, this.state.newTitle);
+    this.setState({ editingTitle: false, newTitle: "" });
+  };
+
+  onChangeTitleHandler = event => {
+    this.setState({ newTitle: event.target.value });
+  };
+
+  render() {
+    return this.props.connectDropTarget(
+      <div className="List scrollbar">
+        <div className="List-Header">
+          <EditableTitle
+            editingTitle={this.state.editingTitle}
+            title={this.props.list.title}
+            newTitle={this.state.newTitle}
+            toggleEditing={() => this.toggleProp("editingTitle")}
+            onSubmitTitleHandler={this.onSubmitTitleHandler}
+            onChangeTitleHandler={this.onChangeTitleHandler}
+          />
+          <button
+            className="Add-Card-Button"
+            onClick={() => this.toggleProp("adding")}
+          >
+            <i className="fas fa-plus" />
+          </button>
+        </div>
+        <div className="List-Cards">
+          {this.state.adding ? (
+            <NewCard
+              value={this.state.newCardText}
+              onChange={this.onCardChangeHandler}
+              onSubmitHandler={this.onSubmitCardHandler}
+              listIndex={this.props.index}
+            />
+          ) : null}
+          {this.props.list.cards.map(card => (
+            <Card key={`card-${card.id}`} {...card} />
+          ))}
+          {this.props.isOver ? <div className="Card" /> : null}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default DropTarget("CARD", listTarget, collect)(List);
