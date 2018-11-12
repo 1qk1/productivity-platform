a7d306221844ce52d049c3744fff80fea56c8f49
Panagiotis <1qk231@gmail.com>
2018-11-12 18:26:55 +0200

    add hero component

---
+++ webclient/src/components/UnauthorizedSections/Hero/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Hero.scss";
+
+const hero = () => (
+  <section className="Section Hero">
+    <div className="Container">
+      <h1>I don't even know</h1>
+    </div>
+  </section>
+);
+
+export default hero;
