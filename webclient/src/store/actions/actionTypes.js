c3386da30a7aedb8a5bae55adc6a012e499d6d76
Panagiotis <1qk231@gmail.com>
2018-11-13 20:54:33 +0200

    add pomodoro action types

--- webclient/src/store/actions/actionTypes.js
+++ webclient/src/store/actions/actionTypes.js
@@ -6,6 +6,8 @@ export const AUTH_LOGOUT = "AUTH_LOGOUT";
 export const START_TIMER = "START_TIMER";
 export const DEC_TIMER = "DEC_TIMER";
 export const STOP_TIMER = "STOP_TIMER";
+export const POMODORO_COMPLETED = "POMODORO_COMPLETED";
+export const GET_POMODOROS = "GET_POMODOROS";
 
 export const ADD_LIST = "ADD_LIST";
 export const ADD_CARD = "ADD_CARD";
