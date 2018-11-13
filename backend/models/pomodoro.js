299d27e01c54d052111b023ed0be7e2080bdb23d
Panagiotis <1qk231@gmail.com>
2018-11-13 20:52:49 +0200

    add basic pomodoro schema

--- backend/models/pomodoro.js
+++ backend/models/pomodoro.js
@@ -1,7 +1,12 @@
-const mongoose = require("mongoose"),
-  bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
 
-const pomodoroSchema = new mongoose.Schema({});
+const pomodoroSchema = new mongoose.Schema({
+  userId: mongoose.SchemaTypes.ObjectId,
+  date: {
+    type: Date,
+    default: new Date()
+  }
+});
 
 const Pomodoro = mongoose.model("pomodoro", pomodoroSchema);
 
