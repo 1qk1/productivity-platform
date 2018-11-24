e9be80835a0ae42bc5eb66edb55b5b28eba256b3
Panagiotis <1qk231@gmail.com>
2018-11-24 16:20:06 +0200

    remove try-catch, move password verification to new function

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,35 +1,38 @@
 const router = require("express").Router(),
   User = require("../models/user"),
-  bcrypt = require("bcrypt"),
-  passport = require("passport"),
-  sendJSONResponse = require("../handlers/auth").sendJSONResponse;
+  sendJSONResponse = require("../handlers/auth").sendJSONResponse,
+  verifyPassword = require("../middleware").verifyPassword;
 
 router.post("/register", async (req, res) => {
   const user = req.body;
-  try {
-    // check if user exists
-    const searchedUser = await User.findOne({ username: user.username });
-    if (searchedUser !== null) {
-      throw new Error("User already exists");
-    }
-    // create user
-    User.create(user, (err, newUser) => {
-      if (err !== null) {
-        throw new Error(err);
-      } else {
-        req.user = newUser;
-        sendJSONResponse(req, res);
-      }
-    });
-  } catch (error) {
-    res.sendStatus(400);
+  // check if user exists
+  const searchedUser = await User.findOne({ username: user.username });
+  // if there is already a user with that username,
+  // send an error and message
+  if (searchedUser !== null) {
+    return res.sendStatus(400).send("User already exists.");
   }
+  // create user
+  User.create(user, (err, newUser) => {
+    if (err !== null) {
+      // if there is any error, send it to the client
+      res.sendStatus(400).send("We couldn't create a user.");
+    } else {
+      // else
+      // add new user's username and id
+      // in req.user
+      req.user = newUser;
+      // and send a JWT response
+      sendJSONResponse(req, res);
+    }
+  });
 });
 
 router.post(
   "/login",
   // check the credentials sent by the user
-  passport.authenticate("local", { session: false }),
+  verifyPassword,
+  // if they correct, send a JWT response
   sendJSONResponse
 );
 
