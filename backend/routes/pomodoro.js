010e2d5ba37fcf15cdeed89d19058e656d4761e5
Panagiotis <1qk231@gmail.com>
2018-11-24 11:34:28 +0200

    remove unused try-catch blocks

--- backend/routes/pomodoro.js
+++ backend/routes/pomodoro.js
@@ -5,32 +5,24 @@ const router = require("express").Router(),
 router.post("/", middleware.verifyToken, (req, res) => {
   const userId = req.user.id;
   console.log(userId);
-  try {
-    Pomodoro.create({ userId }, (error, newPomodoro) => {
-      if (error) {
-        throw new Error(error);
-      } else {
-        res.json(newPomodoro);
-      }
-    });
-  } catch (error) {
-    res.send(error);
-  }
+  Pomodoro.create({ userId }, (error, newPomodoro) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.json(newPomodoro);
+    }
+  });
 });
 
 router.get("/", middleware.verifyToken, (req, res) => {
   const userId = req.user.id;
-  try {
-    Pomodoro.find({ userId }, (error, pomodoros) => {
-      if (error) {
-        throw new Error(error);
-      } else {
-        res.json(pomodoros);
-      }
-    });
-  } catch (error) {
-    res.send(error);
-  }
+  Pomodoro.find({ userId }, (error, pomodoros) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.json(pomodoros);
+    }
+  });
 });
 
 module.exports = router;
