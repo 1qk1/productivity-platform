f1871bc48ffd3ead5254f088df4bf8103c257469
Panagiotis <1qk231@gmail.com>
2018-12-17 18:58:58 +0200

    split board routes into 2 files

---
+++ backend/routes/boardCard.js
@@ -0,0 +1,85 @@
+const router = require("express").Router(),
+  middleware = require("../middleware"),
+  BoardList = require("../models/boardList"),
+  BoardCard = require("../models/boardCard");
+
+router.put("/", middleware.verifyToken, (req, res) => {
+  // find by id and update
+  BoardCard.findByIdAndUpdate(
+    // id
+    req.body.id,
+    //   // properties to change
+    { ...req.body.edit },
+    // don't use deprecated function
+    { useFindAndModify: false },
+    (error, found) => {
+      // if error send error
+      if (error) {
+        return res.status(400).send("Something went wrong");
+      }
+      // update the object to send it back to the user
+      // found has the old text although it has been
+      // updated in the database
+      const updated = { ...found.toObject(), ...req.body.edit };
+      // send it back to the user
+      res.json({ updatedCard: updated });
+    }
+  );
+});
+
+router.post("/", middleware.verifyToken, (req, res) => {
+  // deconstruct the data we need to make a card
+  const { text, listId } = req.body;
+  // create the card data
+  const cardData = {
+    text,
+    listId,
+    userId: req.user.id
+  };
+  // create card
+  BoardCard.create(cardData, (error, newCard) => {
+    if (error) {
+      return res.status(400).send("Something went wrong");
+    } else {
+      BoardList.findById(listId, (error, list) => {
+        if (error) {
+          return res.status(400).send("Something went wrong");
+        } else {
+          list.cards.push(newCard);
+          list.save();
+          res.json({ newCard });
+        }
+      });
+    }
+  });
+});
+
+router.delete("/:listId/:cardId", middleware.verifyToken, (req, res) => {
+  // deconstruct the data we need to delete a card
+  const { listId, cardId } = req.params;
+  // delete card
+  BoardCard.deleteOne({ _id: cardId }, error => {
+    if (error) {
+      return res.status(400).send("Something went wrong");
+    } else {
+      // delete card reference from list entry
+      BoardList.findById(listId, (error, list) => {
+        if (error) {
+          return res.status(400).send("Something went wrong");
+        } else {
+          // find index
+          const cardIndex = list.cards.findIndex(
+            card => card._id.toString() === cardId
+          );
+          // delete card
+          list.cards.splice(cardIndex, 1);
+          // save
+          list.save();
+          res.json({ done: true });
+        }
+      });
+    }
+  });
+});
+
+module.exports = router;
--- backend/routes/boardList.js
+++ backend/routes/boardList.js
@@ -4,7 +4,7 @@ const router = require("express").Router(),
   BoardCard = require("../models/boardCard"),
   User = require("../models/user");
 
-router.get("/list", middleware.verifyToken, (req, res) => {
+router.get("/", middleware.verifyToken, (req, res) => {
   // find all lists for current logged in user
   BoardList.find({ userId: req.user.id })
     // populate cards (if any)
@@ -20,7 +20,7 @@ router.get("/list", middleware.verifyToken, (req, res) => {
     });
 });
 
-router.post("/list", middleware.verifyToken, (req, res) => {
+router.post("/", middleware.verifyToken, (req, res) => {
   // get the number of lists for the logged in user
   BoardList.countDocuments({ userId: req.user.id }, (error, listCount) => {
     if (error) {
@@ -55,7 +55,7 @@ router.post("/list", middleware.verifyToken, (req, res) => {
   });
 });
 
-router.put("/list", middleware.verifyToken, (req, res) => {
+router.put("/", middleware.verifyToken, (req, res) => {
   // find by id and update
   BoardList.findByIdAndUpdate(
     // id
@@ -79,86 +79,7 @@ router.put("/list", middleware.verifyToken, (req, res) => {
   );
 });
 
-router.put("/card", middleware.verifyToken, (req, res) => {
-  // find by id and update
-  BoardCard.findByIdAndUpdate(
-    // id
-    req.body.id,
-    //   // properties to change
-    { ...req.body.edit },
-    // don't use deprecated function
-    { useFindAndModify: false },
-    (error, found) => {
-      // if error send error
-      if (error) {
-        return res.status(400).send("Something went wrong");
-      }
-      // update the object to send it back to the user
-      // found has the old text although it has been
-      // updated in the database
-      const updated = { ...found.toObject(), ...req.body.edit };
-      // send it back to the user
-      res.json({ updatedCard: updated });
-    }
-  );
-});
-
-router.post("/card", middleware.verifyToken, (req, res) => {
-  // deconstruct the data we need to make a card
-  const { text, listId } = req.body;
-  // create the card data
-  const cardData = {
-    text,
-    listId,
-    userId: req.user.id
-  };
-  // create card
-  BoardCard.create(cardData, (error, newCard) => {
-    if (error) {
-      return res.status(400).send("Something went wrong");
-    } else {
-      BoardList.findById(listId, (error, list) => {
-        if (error) {
-          return res.status(400).send("Something went wrong");
-        } else {
-          list.cards.push(newCard);
-          list.save();
-          res.json({ newCard });
-        }
-      });
-    }
-  });
-});
-
-router.delete("/card/:listId/:cardId", middleware.verifyToken, (req, res) => {
-  // deconstruct the data we need to delete a card
-  const { listId, cardId } = req.params;
-  // delete card
-  BoardCard.deleteOne({ _id: cardId }, error => {
-    if (error) {
-      return res.status(400).send("Something went wrong");
-    } else {
-      // delete card reference from list entry
-      BoardList.findById(listId, (error, list) => {
-        if (error) {
-          return res.status(400).send("Something went wrong");
-        } else {
-          // find index
-          const cardIndex = list.cards.findIndex(
-            card => card._id.toString() === cardId
-          );
-          // delete card
-          list.cards.splice(cardIndex, 1);
-          // save
-          list.save();
-          res.json({ done: true });
-        }
-      });
-    }
-  });
-});
-
-router.delete("/list/:listId", middleware.verifyToken, (req, res) => {
+router.delete("/:listId", middleware.verifyToken, (req, res) => {
   // deconstruct the data we need to delete a list
   const { listId } = req.params;
   // delete all cards under a list
