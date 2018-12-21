9cab623fe5b1cdbd25e1ccb4b75e4c139a3e8bbb
Panagiotis <1qk231@gmail.com>
2018-12-21 22:02:54 +0200

    replace callbacks with promises

--- backend/handlers/boardCard.js
+++ backend/handlers/boardCard.js
@@ -11,21 +11,17 @@ const newCardHandler = (req, res) => {
     userId: req.user.id
   };
   // create card
-  BoardCard.create(cardData, (error, newCard) => {
-    if (error) {
-      return res.status(400).send("Something went wrong");
-    } else {
-      BoardList.findById(listId, (error, list) => {
-        if (error) {
-          return res.status(400).send("Something went wrong");
-        } else {
+  BoardCard.create(cardData)
+    .then(newCard => {
+      BoardList.findById(listId)
+        .then(list => {
           list.cards.push(newCard);
           list.save();
           res.json({ newCard });
-        }
-      });
-    }
-  });
+        })
+        .catch(error => res.handleError(error));
+    })
+    .catch(error => res.handleError(error));
 };
 
 const editCardHandler = (req, res) => {
@@ -36,35 +32,28 @@ const editCardHandler = (req, res) => {
     //   // properties to change
     { ...req.body.edit },
     // don't use deprecated function
-    { useFindAndModify: false },
-    (error, found) => {
-      // if error send error
-      if (error) {
-        return res.status(400).send("Something went wrong");
-      }
+    { useFindAndModify: false }
+  )
+    .then(found => {
       // update the object to send it back to the user
       // found has the old text although it has been
       // updated in the database
       const updated = { ...found.toObject(), ...req.body.edit };
       // send it back to the user
       res.json({ updatedCard: updated });
-    }
-  );
+    })
+    .catch(error => res.handleError(error));
 };
 
 const deleteCardHandler = (req, res) => {
   // deconstruct the data we need to delete a card
   const { listId, cardId } = req.params;
   // delete card
-  BoardCard.deleteOne({ _id: cardId }, error => {
-    if (error) {
-      return res.status(400).send("Something went wrong");
-    } else {
+  BoardCard.deleteOne({ _id: cardId })
+    .then(() => {
       // delete card reference from list entry
-      BoardList.findById(listId, (error, list) => {
-        if (error) {
-          return res.status(400).send("Something went wrong");
-        } else {
+      BoardList.findById(listId)
+        .then(list => {
           // find index
           const cardIndex = list.cards.findIndex(
             card => card._id.toString() === cardId
@@ -74,10 +63,10 @@ const deleteCardHandler = (req, res) => {
           // save
           list.save();
           res.json({ done: true });
-        }
-      });
-    }
-  });
+        })
+        .catch(error => res.handleError(error));
+    })
+    .catch(error => res.handleError(error));
 };
 
 module.exports = {
