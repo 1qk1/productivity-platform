0453a3b52085fefa35cffaa5418e4de2097b06c4
Panagiotis <1qk231@gmail.com>
2018-12-24 05:40:30 +0200

    add changeCardList route handler

--- backend/handlers/boardCard.js
+++ backend/handlers/boardCard.js
@@ -50,26 +50,87 @@ const editCardHandler = (req, res) => {
     .catch(error => res.handleError(error));
 };
 
+const changeCardList = (req, res) => {
+  const { fromList, toList, cardId } = req.body;
+  // get the fromList and toList from the db
+  // remove the card from fromlist
+  // add the card to toList
+  // get the card itself
+  // update listId to the new list id
+  // send back the edited card
+  BoardList.find({
+    _id: { $in: [fromList, toList] }
+  })
+    .then(found => {
+      BoardCard.findById(cardId).then(cardToMove => {
+        // console.log(found, "found");
+        const [from, to] =
+          found[0]._id.toString() === fromList
+            ? [found[0], found[1]]
+            : [found[1], found[0]];
+        const cardindex = from.cards.findIndex(
+          card => card._id.toString() === cardId
+        );
+        from.cards.splice(cardindex, 1);
+        to.cards.push(cardToMove);
+        cardToMove.listId = to._id;
+        Promise.all([from.save(), to.save(), cardToMove.save()])
+          .then(() => {
+            res.sendStatus(200);
+          })
+          .catch(res.handleError);
+      });
+    })
+    .catch(res.handleError);
+  // console.log(req.body);
+};
+
+// const deleteCardHandler = (req, res) => {
+//   // deconstruct the data we need to delete a card
+//   const { listId, cardId } = req.params;
+//   // delete card
+//   BoardCard.deleteOne({ _id: cardId })
+//     .then(() => {
+//       // delete card reference from list entry
+//       BoardList.findByIdAndUpdate(
+//         listId,
+//         // pull from the list of cards, all values
+//         // that equal cardId
+//         // the card we want to remove will
+//         // be an ObjectId reference so it will
+//         // match with cardId
+//         { $pull: { cards: cardId } },
+//         { useFindAndModify: false }
+//       )
+//         .then(() => {
+//           // send reponse that everything was done
+//           res.sendStatus(200);
+//         })
+//         .catch(error => res.handleError(error));
+//     })
+//     .catch(error => res.handleError(error));
+// };
+
 const deleteCardHandler = (req, res) => {
   // deconstruct the data we need to delete a card
   const { listId, cardId } = req.params;
   // delete card
-  BoardCard.deleteOne({ _id: cardId })
+  BoardList.findByIdAndUpdate(
+    listId,
+    // pull from the list of cards, all values
+    // that equal cardId
+    // the card we want to remove will
+    // be an ObjectId reference so it will
+    // match with cardId
+    { $pull: { cards: cardId } },
+    { useFindAndModify: false }
+  )
     .then(() => {
-      // delete card reference from list entry
-      BoardList.findByIdAndUpdate(
-        listId,
-        // pull from the list of cards, all values
-        // that equal cardId
-        // the card we want to remove will
-        // be an ObjectId reference so it will
-        // match with cardId
-        { $pull: { cards: cardId } },
-        { useFindAndModify: false }
-      )
+      // send reponse that everything was done
+      res.sendStatus(200);
+      BoardCard.deleteOne({ _id: cardId })
         .then(() => {
-          // send reponse that everything was done
-          res.sendStatus(200);
+          // delete card reference from list entry
         })
         .catch(error => res.handleError(error));
     })
@@ -79,5 +140,6 @@ const deleteCardHandler = (req, res) => {
 module.exports = {
   newCardHandler,
   editCardHandler,
-  deleteCardHandler
+  deleteCardHandler,
+  changeCardList
 };
