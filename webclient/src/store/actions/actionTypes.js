d3c1618403b931f99d65f95d95aaa0f91c1be165
Panagiotis <1qk231@gmail.com>
2019-01-09 21:16:47 +0200

    add UPDATE_TIMER action type

--- webclient/src/store/actions/actionTypes.js
+++ webclient/src/store/actions/actionTypes.js
@@ -6,6 +6,7 @@ export const AUTH_LOGOUT = "AUTH_LOGOUT";
 export const START_TIMER = "START_TIMER";
 export const START_BREAK = "START_BREAK";
 export const DEC_TIMER = "DEC_TIMER";
+export const UPDATE_TIMER = "UPDATE_TIMER";
 export const STOP_TIMER = "STOP_TIMER";
 export const POMODORO_COMPLETED = "POMODORO_COMPLETED";
 export const GET_POMODOROS = "GET_POMODOROS";
