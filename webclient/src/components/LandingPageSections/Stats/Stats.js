f078a8d9c94b82266f52037bec2d8d58f99d2e1e
Panagiotis <1qk231@gmail.com>
2019-02-02 21:06:34 +0200

    add a stats section in the landing page

---
+++ webclient/src/components/LandingPageSections/Stats/Stats.js
@@ -0,0 +1,26 @@
+import React from "react";
+import statsImage from "../../../assets/landing-stats.svg";
+
+const stats = () => {
+  return (
+    <section className="Section Section-Stats">
+      <div className="Container row">
+        <div className="Subsection col m8">
+          <h2 className="Subsection-Header">
+            See your most productive hours and more stats.
+          </h2>
+          <p className="Subsection-Paragraph">
+            Track your productivity and see when you are most productive, review
+            how you manage your time and manage your time better.{" "}
+            <small style={{ color: "gray" }}>(coming soon)</small>
+          </p>
+        </div>
+        <div className="Subsection col m4">
+          <img className="Subsection-Image" src={statsImage} alt="Clock Icon" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default stats;
