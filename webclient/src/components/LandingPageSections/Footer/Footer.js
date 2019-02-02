b8573a2ac02df5d8caa0f9390959cd3bb1a2deff
Panagiotis <1qk231@gmail.com>
2019-02-02 21:07:51 +0200

    add footer section

---
+++ webclient/src/components/LandingPageSections/Footer/Footer.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Footer.scss";
+
+const footer = () => {
+  return (
+    <section className="Landing-Footer">
+      <div className="Container">
+        <ul className="Footer-Links">
+          <li>
+            <a
+              href="https://twitter.com/qktweets"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Twitter
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/1qk1/productivity-platform"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Github
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default footer;
