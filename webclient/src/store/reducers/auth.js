b4a9ff618cf04ba96cc3d24335d88e8a880fbfd0
Panagiotis <1qk231@gmail.com>
2019-04-07 15:42:04 +0300

    remove unused reducer cases

--- webclient/src/store/reducers/auth.js
+++ webclient/src/store/reducers/auth.js
@@ -6,7 +6,6 @@ const initialState = {
   // null = not logged in
   token: undefined,
   user: null,
-  loading: false,
   // read error message on error.response.data
   error: null
 };
@@ -14,18 +13,13 @@ const initialState = {
 export default (state = initialState, action) => {
   let newState = _.cloneDeep(state);
   switch (action.type) {
-    case actionTypes.AUTH_START:
-      return { ...state, loading: true, error: null };
     case actionTypes.AUTH_SUCCESS:
       return {
         ...state,
-        loading: false,
         error: null,
         token: action.token,
         user: action.user
       };
-    case actionTypes.AUTH_FAIL:
-      return { ...state, loading: false, error: action.error };
     case actionTypes.AUTH_LOGOUT:
       return { ...state, token: null, user: null };
     case actionTypes.ADD_EXTENSION: {
