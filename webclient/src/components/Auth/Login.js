1fefc38854895f87dfa0865db1d6dc8207b20810
Panagiotis <1qk231@gmail.com>
2019-04-07 15:38:27 +0300

    improve auth forms and use InputWithErrors

--- webclient/src/components/Auth/Login.js
+++ webclient/src/components/Auth/Login.js
@@ -1,44 +1,40 @@
 import React from "react";
 import PropTypes from "prop-types";
+import InputWithErrors from "../UI/InputWithErrors/InputWithErrors";
 
 import "./Auth.scss";
 
-const login = props => {
-  return (
-    <div className="LoginForm AuthForm">
-      <h5>Login</h5>
-      <form id="login" onSubmit={props.submitHandler}>
-        <label>
-          Username
-          <input
-            value={props.username}
-            onChange={props.onChangeHandler}
-            type="text"
-            name="username"
-          />
-        </label>
-        <label>
-          Password
-          <input
-            value={props.password}
-            onChange={props.onChangeHandler}
-            type="password"
-            name="password"
-          />
-        </label>
-        <button className="btn btn-green" type="submit" name="action">
-          Log in
-        </button>
-      </form>
-    </div>
-  );
-};
+const login = props => (
+  <div className="LoginForm AuthForm">
+    <h5>Login</h5>
+    <form id="login" onSubmit={props.submitHandler}>
+      <InputWithErrors
+        label="Username"
+        data={props.fieldData.username}
+        onChange={props.onChangeHandler}
+        type="text"
+        name="username"
+        formId="login"
+      />
+      <InputWithErrors
+        label="Password"
+        data={props.fieldData.password}
+        onChange={props.onChangeHandler}
+        type="password"
+        name="password"
+        formId="login"
+      />
+      <button className="btn btn-green" type="submit" name="action">
+        Log in
+      </button>
+    </form>
+  </div>
+);
 
 login.propTypes = {
   submitHandler: PropTypes.func.isRequired,
   onChangeHandler: PropTypes.func.isRequired,
-  username: PropTypes.string.isRequired,
-  password: PropTypes.string.isRequired
+  fieldData: PropTypes.object.isRequired
 };
 
 export default login;
--- webclient/src/components/Auth/Register.js
+++ webclient/src/components/Auth/Register.js
@@ -1,54 +1,48 @@
 import React from "react";
 import PropTypes from "prop-types";
+import InputWithErrors from "../UI/InputWithErrors/InputWithErrors";
 
 import "./Auth.scss";
 
-const register = props => {
-  return (
-    <div className="SignupForm AuthForm">
-      <h5>Signup</h5>
-      <form id="register" onSubmit={props.submitHandler}>
-        <label>
-          Username
-          <input
-            onChange={props.onChangeHandler}
-            type="text"
-            name="username"
-            value={props.username}
-          />
-        </label>
-        <label>
-          Password
-          <input
-            onChange={props.onChangeHandler}
-            type="password"
-            name="password"
-            value={props.password}
-          />
-        </label>
-        <label>
-          Email
-          <input
-            onChange={props.onChangeHandler}
-            type="email"
-            name="email"
-            value={props.email}
-          />
-        </label>
-        <button className="btn btn-green" type="submit" name="action">
-          Sign Up
-        </button>
-      </form>
-    </div>
-  );
-};
+const register = props => (
+  <div className="SignupForm AuthForm">
+    <h5>Signup</h5>
+    <form id="register" onSubmit={props.submitHandler}>
+      <InputWithErrors
+        label="Username"
+        data={props.fieldData.username}
+        onChange={props.onChangeHandler}
+        type="text"
+        name="username"
+        formId="register"
+      />
+      <InputWithErrors
+        label="Password"
+        data={props.fieldData.password}
+        onChange={props.onChangeHandler}
+        type="password"
+        name="password"
+        formId="register"
+      />
+      <InputWithErrors
+        label="Email Address"
+        data={props.fieldData.email}
+        onChange={props.onChangeHandler}
+        type="email"
+        name="email"
+        formId="register"
+      />
+      <button className="btn btn-green" type="submit" name="action">
+        Sign Up
+      </button>
+    </form>
+  </div>
+);
 
 register.propTypes = {
   submitHandler: PropTypes.func.isRequired,
   onChangeHandler: PropTypes.func.isRequired,
-  username: PropTypes.string.isRequired,
-  password: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired
+  fieldData: PropTypes.object.isRequired
 };
 
 export default register;
