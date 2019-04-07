f26fc2687a5fd5654101302f8299cf6c9d236cbf
Panagiotis <1qk231@gmail.com>
2019-04-07 15:34:22 +0300

    add validate function in utilities

---
+++ webclient/src/shared/utilities.js
@@ -0,0 +1,28 @@
+import { isLength, isEmpty, isEmail } from "validator";
+
+export const returnDoubleNumber = num => (num < 10 ? `0${num}` : String(num));
+
+export const secondsToTime = time => {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  return `${returnDoubleNumber(minutes)}:${returnDoubleNumber(seconds)}`;
+};
+
+export const validate = (value, rules) => {
+  const errors = [];
+
+  if (rules.required && isEmpty(value)) {
+    errors.push("This field is required");
+  }
+
+  if (rules.isEmail && !isEmail(value)) {
+    errors.push("Please enter a valid email address");
+  }
+
+  if (rules.minLength && !isLength(value, rules.minLength)) {
+    errors.push(
+      `This field should be more than ${rules.minLength} characters long`
+    );
+  }
+  return errors;
+};
