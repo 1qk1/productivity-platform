ca1da9a037874031b89cdb10f023c732038d3b10
Panagiotis <1qk231@gmail.com>
2019-04-11 21:44:35 +0300

    add axios error interceptor

--- webclient/src/axios.js
+++ webclient/src/axios.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const port = process.env.PORT || 3001;
 const ip = process.env.IP || "localhost";
@@ -10,7 +11,18 @@ if (process.env.NODE_ENV === "production") {
 }
 
 const instance = axios.create({
-  baseURL: `${url}/api`
+  baseURL: `${url}/api`,
+  timeout: 5000
 });
+
+instance.interceptors.response.use(
+  config => config,
+  error => {
+    if (error.code === "ECONNABORTED" || error.response === undefined) {
+      toast.error("There was a problem contacting the server");
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
