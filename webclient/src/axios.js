c2a3a914a1fa75af333b1a99bf6cfe3db516f189
1qk1 <1qk231@gmail.com>
2019-11-11 11:08:00 +0200

    stringify data in axios

--- webclient/src/axios.js
+++ webclient/src/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { toast } from "react-toastify";
+import qs from "qs";
 
 const port = process.env.REACT_APP_PORT || 3001;
 const ip = process.env.REACT_APP_IP || "localhost";
@@ -9,11 +10,29 @@ let url = `${protocol}://${ip}:${port}`;
 
 const instance = axios.create({
   baseURL: `${url}/api`,
-  timeout: 10000
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded"
+  }
 });
 
+instance.interceptors.request.use(
+  config => {
+    // Do something before request is sent
+    console.log(config);
+    return { ...config, data: qs.stringify(config.data) };
+  },
+  error => {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
 instance.interceptors.response.use(
-  config => config,
+  response => {
+    console.log(response);
+    return { ...response, data: qs.parse(response.data) };
+  },
   error => {
     if (error.code === "ECONNABORTED" || error.response === undefined) {
       toast.error("There was a problem contacting the server");
