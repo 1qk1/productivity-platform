592fcbc4ab2114aa5f3b03c14f1fa07e2d86d7e4
Panagiotis <1qk231@gmail.com>
2020-07-25 20:54:22 +0300

    Don't start another pomodoro when break completes

--- webclient/src/store/actions/pomodoro.js
+++ webclient/src/store/actions/pomodoro.js
@@ -5,7 +5,7 @@ import PomodoroSound from "../../assets/notifications/pomodoro.mp3";
 import BreakSound from "../../assets/notifications/break.mp3";
 
 export const startPomodoro = () => {
-  return dispatch => {
+  return (dispatch) => {
     const decTimer = () => dispatch({ type: actionTypes.DEC_TIMER });
     const intervalId = setInterval(decTimer, 1000);
     dispatch({ type: actionTypes.START_TIMER, intervalId });
@@ -21,49 +21,47 @@ export const updateTimer = () => {
 };
 
 export const stopTimer = () => {
-  return dispatch => {
+  return (dispatch) => {
     dispatch({ type: actionTypes.STOP_TIMER });
   };
 };
 
 export const end5Seconds = () => {
-  return dispatch => {
+  return (dispatch) => {
     dispatch({ type: actionTypes.END_POMODORO_5_SECONDS });
   };
 };
 
 export const submitPomodoro = () => {
-  return dispatch => {
+  return (dispatch) => {
     axios
       .post("/pomodoro")
-      .then(response => {
+      .then((response) => {
         toast.success("Added new pomodoro in the database!");
         const newPomodoro = response.data;
         dispatch({ type: actionTypes.POMODORO_COMPLETED, newPomodoro });
       })
-      .catch(error => {
+      .catch((error) => {
         toast.error(error.response.data.error.message);
       });
   };
 };
 
-export const pomodoroCompleted = isPomodoro => {
-  return dispatch => {
+export const pomodoroCompleted = (isPomodoro) => {
+  return (dispatch) => {
     const soundFile = isPomodoro ? PomodoroSound : BreakSound;
     const sound = new Audio(soundFile);
     sound.play();
     if (isPomodoro) {
       dispatch(submitPomodoro());
       dispatch({ type: actionTypes.START_BREAK });
-    } else {
-      dispatch({ type: actionTypes.START_TIMER });
     }
   };
 };
 
 export const getPomodoros = () => {
-  return dispatch => {
-    axios.get("/pomodoro/").then(response => {
+  return (dispatch) => {
+    axios.get("/pomodoro/").then((response) => {
       const pomodoros = response.data;
       dispatch({ type: actionTypes.GET_POMODOROS, pomodoros });
     });
